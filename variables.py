55bbef000b3f185ca6d0a246b8a55186945abe28
Kanchana S A <kanchanasa815@gmail.com>
2026-07-15 11:47:17 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -20,3 +20,23 @@ b=9
 print(a,b)
 a,b=b,a
 print(a,b)
+
+#find the data type of a variable
+
+a="string"
+b=0.99
+c=90
+d=True
+e=[1,2,3,4]
+f=(4,5,6,7)
+g={"apple","banana","graps"}
+h={"name":"kanchana","age":22,"stading":"B.E"}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(e))
+print(type(h))
