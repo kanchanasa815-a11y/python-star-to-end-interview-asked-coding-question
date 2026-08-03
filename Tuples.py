765270c73e43d5342588408328c1f7a3b839c6f1
Kanchana S A <kanchanasa815@gmail.com>
2026-08-03 19:55:11 +0530

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -5,3 +5,7 @@ print(largest)
     (or)
 numbers=(20,30,17,40,67)
 print(max(numbers))
+#Count Occurrences    ==>Occurrences means how many time the number will oppears
+numbers=(2,2,2,3,4,5,2,5,7,3,2,9)
+print(numbers.count(2))
+
